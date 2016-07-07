286ff1104ece9893c92d0b615d9132ca09943bbf
dyslexie <dag.rybak@gmail.com>
2016-07-07 22:28:35 +0200

    Dodanie sprawdzania poprawności danych.

--- Zad1/Zad4/Program.cs
+++ Zad1/Zad4/Program.cs
@@ -12,9 +12,10 @@ namespace Zad5
 
         private static void GuessTheNumber()
         {
-            byte number;
-            byte userNumber;
+            int number;
+            int userNumber;
             int attempt = 0;
+            bool isConvertable = false;
 
             Random randomNumber = new Random();
 
@@ -23,16 +24,22 @@ namespace Zad5
             do
             {
                 if (attempt >= 1)
-                    Console.WriteLine("Tym razem komputer Cię przechytrzył. Spróbuj jeszcze raz! :)")
+                    Console.WriteLine("Tym razem komputer Cię przechytrzył. Spróbuj jeszcze raz! :)");
 
-                number = Convert.ToByte(randomNumber.Next(1, 10));
+                number = randomNumber.Next(1, 10);
                 Console.WriteLine("Komputer wylosował już liczbę z zakresu 1 do 10. Jaka to liczba?");
-                userNumber = Convert.ToByte(Console.ReadLine());
+
+                isConvertable = Int32.TryParse(Console.ReadLine(), out userNumber);
+
+                if (isConvertable == false || userNumber>10 || userNumber<1)
+                    Console.WriteLine("Podano nieprawidłowe dane.");
+
                 ++attempt;
             }
             while (number != userNumber);
 
             Console.WriteLine("Brawo, udało Ci się odgadnąć wylosowaną liczbę za " + attempt + " podejściem!");
+            Console.WriteLine("\nCzy chcesz zagrać jeszcze raz? \n1 - TAK; 0 - NIE");
         }
     }
 }
